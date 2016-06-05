e7ec33f95c44577c0e5c531dfa268713eddbdd03
yadieet <qts19bit@gmail.com>
2016-06-05 12:02:40 +0700

    add `singleton`

---
+++ singleton/Foo.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016 Yadieet SA <yadieet@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package singleton;
+
+/**
+ * Created by yadieet on 05/06/16.
+ */
+final class Foo
+{
+    private static final Foo instance;
+
+    static
+    {
+        //...
+        instance = new Foo();
+    }
+
+    private Foo()
+    {
+        //...
+    }
+
+
+    static Foo getInstance()
+    {
+        return instance;
+    }
+
+}
